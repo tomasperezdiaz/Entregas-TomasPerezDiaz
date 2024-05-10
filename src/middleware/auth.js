bc79ded4575f40e6528727858660976d7088a25b
Tomas Perez Diaz <tomasperezdiaz03@gmail.com>
2024-05-10 11:46:12 -0600

    Arreglo

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,3 +4,8 @@ export const auth = (req = request, res = response, next) => {
   if (req.session?.user) return next();
   return res.redirect("/login");
 };
+
+export const admin = (req = request, res = response, next) => {
+    if (req.session?.rol == "admin") return next();
+    return res.redirect("/login");
+  };
